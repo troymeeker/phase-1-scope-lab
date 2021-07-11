391148683ac57df6770a7c7dbee7ce8b5734246b
troymeeker <troymeeker1@gmail.com>
2021-07-11 15:09:53 -0700

    commit tree a4a03dc88cd0eb280746eed25315c445c16f0607

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Write your solution in this file!
+var customerName = 'bob';
+var bestCustomer= 'not bob';
+
+function upperCaseCustomerName(){
+    return customerName.toUpperCase();
+    
+};
+upperCaseCustomerName();
+
+function setBestCustomer(){
+    return bestCustomer;
+}
